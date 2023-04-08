8b4e56c4c2e3b00e34dfb3be10d63a462a4c441c
HassarEl <mido.hass.31@gmail.com>
2023-04-08 15:33:39 +0000

    add user to group user management

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -24,6 +24,7 @@ class UserResource extends Resource
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-user';
+    protected static ?string $navigationGroup = 'user management';
 
     public static function form(Form $form): Form
     {
